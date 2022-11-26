221fd09f38619f4f786adca8fdb77ae0d6fea274
André Henrique Rodrigues da Silva <andrehrsilva@gmail.com>
2022-11-26 11:29:45 -0300

    Update setupfedora.sh
    atualizando

--- setupfedora.sh
+++ setupfedora.sh
@@ -5,7 +5,7 @@ sudo dnf update &&
 
 ## Adicionando Python, pip e jupyter
 
-sudo apt install python3 python-pip &&
+sudo dnf install python3 python-pip &&
 
 pip install jupyterlab &&
 
@@ -16,25 +16,20 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 ## Instalando Apps do Flathub ##
 
-sudo flatpak install flathub com.wps.Office -y &&
 sudo flatpak install flathub com.sublimetext.three -y &&
-sudo flatpak install flathub org.telegram.desktop -y &&
 sudo flatpak install flathub com.discordapp.Discord -y &&
 sudo flatpak install flathub com.google.Chrome -y &&
 sudo flatpak install flathub com.microsoft.Teams -y &&
 sudo flatpak install flathub org.videolan.VLC -y &&
 sudo flatpak install flathub app.midterm.MidtermDesktop -y &&
 sudo flatpak install flathub com.github.alainm23.planner -y &&
-sudo flatpak install flathub org.gabmus.whatip -y &&
 sudo flatpak install flathub com.visualstudio.code -y &&
-sudo flatpak install flathub org.sqlitebrowser.sqlitebrowser -y &&
 sudo flatpak install flathub com.slack.Slack -y &&
 sudo flatpak install flathub us.zoom.Zoom -y &&
- 
+sudo flatpak install flathub com.github.flxzt.rnote -y &&
+sudo flatpak install flathub org.gabmus.whatip -y &&
 
-## Softwares que precisam de download externo ##
 
-# cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.rpm && sudo dpkg -i *.rpm && 
 
 
 echo "Finalizado"
